eba48a47c1c4cad399da488648e2a37facc974cd
salman-ctrl <zirmanvictory@gmail.com>
2025-04-27 02:26:02 +0700

    first commit

---
+++ app/Models/OfficeSpacePhoto.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OfficeSpacePhoto extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'photo',
+        'office_space_id',
+    ];
+}
